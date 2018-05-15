bc733663c94b4ea25f4e0d6af89413a0e45c6c53
filipMatusak <filip.matusak@gmail.com>
2018-05-15 20:18:52 +0200

    aa

--- index.js
+++ index.js
@@ -245,10 +245,10 @@ if (process.env.NODE_ENV === "development") {
 }*/
 
 async function startServer() {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('./client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile('./client/build/index.html');
   });
   
 
